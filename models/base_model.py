9a81dc2b0dbf4bfbd6cad478fa081adb3947f328
kenkomu <komukenneth216@gmail.com>
2023-05-14 22:32:31 +0300

    created class BaseModel

--- models/base_model.py
+++ models/base_model.py
@@ -1,1 +1,12 @@
 #!/usr/bin/python3
+"""
+class BaseModel that defines all common attributes/methods for other classes:
+"""
+
+import uuid
+from datetime import datetime
+class BaseModel:
+    def __init__(self):
+        self.id = str(uuid.uuid4())
+        self.created_at = datetime.now()
+        self.updated_at = self.created_at
