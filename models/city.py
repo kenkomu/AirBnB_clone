99405b9bff2d847ab47ade2a846cd23691228ced
kenkomu <komukenneth216@gmail.com>
2023-05-30 13:02:48 +0300

    changes

---
+++ models/city.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+"""Module for city class."""
+from models.base_model import BaseModel
+
+
+class City(BaseModel):
+    name = ""
+    state_id = ""
--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -1,8 +1,6 @@
 #!/usr/bin/python3
-"""
-class FileStorage that serializes instances
-"""
-
+"""Module for FileStorage class."""
+import datetime
 import json
 import os
 
