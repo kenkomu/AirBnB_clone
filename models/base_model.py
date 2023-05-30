468ee4a21d7a39040f05500ac3b1991f66c68977
kenkomu <komukenneth216@gmail.com>
2023-05-30 10:16:58 +0300

    base_model.py

--- models/base_model.py
+++ models/base_model.py
@@ -5,15 +5,20 @@ class BaseModel that defines all common attributes/methods for other classes:
 
 import uuid
 from datetime import datetime
+
+
 class BaseModel:
     def __init__(self):
         self.id = str(uuid.uuid4())
         self.created_at = datetime.now()
         self.updated_at = self.created_at
+
     def update(self):
         self.updated_at = datetime.now()
+
     def save(self):
         self.update()
+
     def to_dict(self):
         """Returns a dictionary representation of an instance."""
         my_dict = self.__dict__.copy()
@@ -21,4 +26,3 @@ class BaseModel:
         my_dict["created_at"] = my_dict["created_at"].isoformat()
         my_dict["updated_at"] = my_dict["updated_at"].isoformat()
         return my_dict
-    
