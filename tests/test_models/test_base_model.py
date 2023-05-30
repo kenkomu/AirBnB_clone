70727d1a97c468d22365b6c2eab5765808be8a48
kenkomu <komukenneth216@gmail.com>
2023-05-30 10:42:30 +0300

    tests/

--- tests/test_models/test_base_model.py
+++ tests/test_models/test_base_model.py
@@ -1,23 +1,37 @@
 #!/usr/bin/python3
 """Unittest module for the BaseModel Class."""
 
-from models.base_model import BaseModel
-from datetime import datetime
-import json
-import os
-import time
 import unittest
-import uuid
+from datetime import datetime
+from models.base_model import BaseModel
 
 
 class TestBaseModel(unittest.TestCase):
-    """Test cases for BaseModel class"""
-    def test_1_instantiation(self):
-        """Tests instantiation of BaseModel class."""
-        bm1 = BaseModel()
-        self.assertIsInstance(bm1, BaseModel)
-        self.assertTrue(hasattr(bm1, "id"))
-        self.assertIsInstance(bm1.id, str)
+    def setUp(self):
+        self.base_model = BaseModel()
+
+    def test_attributes(self):
+        self.assertIsInstance(self.base_model.id, str)
+        self.assertIsInstance(self.base_model.created_at, datetime)
+        self.assertIsInstance(self.base_model.updated_at, datetime)
+
+    def test_save(self):
+        old_updated_at = self.base_model.updated_at
+        self.base_model.save()
+        new_updated_at = self.base_model.updated_at
+        self.assertNotEqual(old_updated_at, new_updated_at)
+
+    def test_to_dict(self):
+        obj_dict = self.base_model.to_dict()
+        self.assertIsInstance(obj_dict, dict)
+        self.assertEqual(obj_dict['__class__'], 'BaseModel')
+        self.assertEqual(obj_dict['created_at'], self.base_model.created_at.isoformat())
+        self.assertEqual(obj_dict['updated_at'], self.base_model.updated_at.isoformat())
+
+    def test_str(self):
+        obj_str = str(self.base_model)
+        expected_str = "[BaseModel] ({}) {}".format(self.base_model.id, self.base_model.__dict__)
+        self.assertEqual(obj_str, expected_str)
 
 
 if __name__ == '__main__':
