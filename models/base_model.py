b706f5f19cca8d5dc4ed12e8eedbe4f28e00e456
kenkomu <komukenneth216@gmail.com>
2023-05-30 11:28:17 +0300

    file_storage

--- models/base_model.py
+++ models/base_model.py
@@ -8,38 +8,29 @@ import uuid
 from datetime import datetime
 from models import storage
 
+from models.__init__ import storage
+
 
 class BaseModel:
+    def save(self):
+        """Updates the public instance attribute updated_at with the current datetime and calls save() method of storage."""
+        self.updated_at = datetime.now()
+        storage.save()
+
     def __init__(self, *args, **kwargs):
+        """Initializes a new instance of BaseModel. If not from a dictionary representation, calls new() method on storage."""
         if kwargs:
             for key, value in kwargs.items():
-                if key != '__class__':
-                    if key in ['created_at', 'updated_at']:
-                        setattr(self, key, datetime.strptime(
-                            value, '%Y-%m-%dT%H:%M:%S.%f'))
-                    else:
-                        setattr(self, key, value)
+                if key != "__class__":
+                    setattr(self, key, value)
+            if "id" not in kwargs:
+                self.id = str(uuid.uuid4())
+            if "created_at" in kwargs:
+                self.created_at = datetime.strptime(kwargs["created_at"], "%Y-%m-%dT%H:%M:%S.%f")
+            if "updated_at" in kwargs:
+                self.updated_at = datetime.strptime(kwargs["updated_at"], "%Y-%m-%dT%H:%M:%S.%f")
         else:
             self.id = str(uuid.uuid4())
             self.created_at = datetime.now()
             self.updated_at = self.created_at
             storage.new(self)
-
-    def save(self):
-        self.updated_at = datetime.now()
-        storage.save()
-
-    def to_dict(self):
-        """Returns a dictionary representation of an instance."""
-        my_dict = self.__dict__.copy()
-        my_dict["__class__"] = type(self).__name__
-        my_dict["created_at"] = my_dict["created_at"].isoformat()
-        my_dict["updated_at"] = my_dict["updated_at"].isoformat()
-        return my_dict
-
-    def __str__(self):
-        return "[{}] ({}) {}".format(
-            type(self).__name__,
-            self.id,
-            self.__dict__
-        )
--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -12,24 +12,30 @@ class FileStorage:
     __objects = {}
 
     def all(self):
+        """Returns the dictionary __objects."""
         return self.__objects
 
     def new(self, obj):
-        key = "{}.{}".format(type(obj).__name__, obj.id)
+        """Sets in __objects the obj with key <obj class name>.id."""
+        key = obj.__class__.__name__ + "." + obj.id
         self.__objects[key] = obj
 
     def save(self):
+        """Serializes __objects to the JSON file."""
         obj_dict = {key: obj.to_dict() for key, obj in self.__objects.items()}
         with open(self.__file_path, "w") as file:
             json.dump(obj_dict, file)
 
     def reload(self):
-        if os.path.isfile(self.__file_path):
+        """Deserializes the JSON file to __objects."""
+        try:
             with open(self.__file_path, "r") as file:
                 obj_dict = json.load(file)
                 for key, value in obj_dict.items():
-                    class_name, obj_id = key.split(".")
-                    module = __import__('models.' + class_name,
-                                        fromlist=[class_name])
+                    class_name = value["__class__"]
+                    module = __import__('models.' + class_name, fromlist=[class_name])
                     cls = getattr(module, class_name)
-                    self.__objects[key] = cls(**value)
+                    obj_instance = cls(**value)
+                    self.__objects[key] = obj_instance
+        except FileNotFoundError:
+            pass
