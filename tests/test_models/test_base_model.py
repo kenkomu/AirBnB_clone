8c60f8c0e71f63c17f2befe134e66203ad861352
kenkomu <komukenneth216@gmail.com>
2023-05-30 07:50:38 +0300

    added the tests

---
+++ tests/test_models/test_base_model.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+"""Unittest module for the BaseModel Class."""
+
+from models.base_model import BaseModel
+from datetime import datetime
+import json
+import os
+import time
+import unittest
+import uuid
+
+class TestBaseModel(unittest.TestCase):
+    """Test cases for BaseModel class"""
+    def test_1_instantiation(self):
+        """Tests instantiation of BaseModel class."""
+        bm1 = BaseModel()
+        self.assertIsInstance(bm1, BaseModel)
+        self.assertTrue(hasattr(bm1, "id"))
+        self.assertIsInstance(bm1.id, str)
+
+if __name__ == '__main__':
+    unittest.main()
