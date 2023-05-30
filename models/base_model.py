e031590a1741c3d8bbbc0ae76c392d240855fea4
kenkomu <komukenneth216@gmail.com>
2023-05-30 11:15:12 +0300

    file_storage

--- models/base_model.py
+++ models/base_model.py
@@ -5,7 +5,7 @@ class BaseModel that defines all common attributes/methods for other classes:
 
 import uuid
 from datetime import datetime
-
+from models import storage
 
 class BaseModel:
     def __init__(self, *args, **kwargs):
@@ -21,9 +21,11 @@ class BaseModel:
             self.id = str(uuid.uuid4())
             self.created_at = datetime.now()
             self.updated_at = self.created_at
+            storage.new(self)
 
     def save(self):
         self.updated_at = datetime.now()
+        storage.save()
 
     def to_dict(self):
         """Returns a dictionary representation of an instance."""
---
+++ models/engine/file_storage.py
@@ -0,0 +1,34 @@
+#!/usr/bin/python3
+"""
+class FileStorage that serializes instances to a JSON file and deserializes JSON file to instances
+"""
+
+import json
+import os
+
+
+class FileStorage:
+    __file_path = "file.json"
+    __objects = {}
+
+    def all(self):
+        return self.__objects
+
+    def new(self, obj):
+        key = "{}.{}".format(type(obj).__name__, obj.id)
+        self.__objects[key] = obj
+
+    def save(self):
+        obj_dict = {key: obj.to_dict() for key, obj in self.__objects.items()}
+        with open(self.__file_path, "w") as file:
+            json.dump(obj_dict, file)
+
+    def reload(self):
+        if os.path.isfile(self.__file_path):
+            with open(self.__file_path, "r") as file:
+                obj_dict = json.load(file)
+                for key, value in obj_dict.items():
+                    class_name, obj_id = key.split(".")
+                    module = __import__('models.' + class_name, fromlist=[class_name])
+                    cls = getattr(module, class_name)
+                    self.__objects[key] = cls(**value)
